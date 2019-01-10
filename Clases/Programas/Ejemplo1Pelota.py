b99f528ff55ec8fa69817ad5c0a1a55531ed8e4c
310126070 <brenner227@hotmail.com>
2019-01-10 16:20:14 -0600

    Primer programa

---
+++ Clases/Programas/Ejemplo1Pelota.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Moctezuma Soto, Fernando
+
+print 34 * 3 - 1/2 * 9.81 * 3 ** 2 # 1/2 calcula la división
+
+print 34 * 3 - 1.0 / 2 * 9.81 * 3 ** 2 # 1.0/2 división flotante
+
+print 34 * 1 - 1.0 / 2 * 9.81 * 1 ** 2
+
+print 34 * 1.5 - 1.0 / 2 * 9.81 * 1.5 ** 2
+
+
+v0 = 34
+
+g = 9.81
+
+t = 5
+
+y = v0 * t - 1.0 / 2 * g * t **2
+
+print y
+
