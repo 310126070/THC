06b408929357d06a0cb6f711572a9b65c736ae64
310126070 <brenner227@hotmail.com>
2019-01-15 00:02:35 -0600

    Corrección

--- Clases/Programas/Diana.py
+++ Clases/Programas/Diana.py
@@ -1,11 +1,11 @@
-def diana(x, y):
-    if (x<5 and (y<10 or y>30)) or (x>25 and (y<10 or y>30)):
+def diana(x,y):
+    if ( x < 5 or x >= 25 ) and ( y < 10 or y >= 30 ):
         return (3)
     else:
-        if (x<5 or x>25) and (y>10 and y>30):
+        if ( x < 5 or x >= 25 ) and ( y >= 10 and y < 30 ):
             return (5)
         else:
-            if (x>5 and x<25) and (y<10 or y>30):
+            if ( x >= 5 and x < 25 ) and ( y < 10 or y >= 30 ):
                 return (7)
             else:
                 return (10)
