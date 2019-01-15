c61648cbe8ab712b225f3ff64bb671a27e541798
310126070 <brenner227@hotmail.com>
2019-01-15 14:03:17 -0600

    intento

--- Clases/Programas/funcion01.py
+++ Clases/Programas/funcion01.py
@@ -13,11 +13,21 @@ def raiz(x):
         b=x/h
     return(b)
 
+def raiz1(x):
+    h=x
+    b=1.0
+    e=0.0001
+    while vabs(b-h)>e:
+        h=(b+h)/2
+        b=x/h
+    return(b)
+
 print raiz(1)
 print raiz(4)
 print raiz(9)
 print raiz(9.1)
 print raiz(100)
+
 
 
 
