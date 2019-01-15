3c162bf57859cd607346992acfaba0dd1e0781c5
310126070 <brenner227@hotmail.com>
2019-01-15 01:36:54 -0600

    Tarea03

---
+++ Clases/Programas/Tarea03.py
@@ -0,0 +1,15 @@
+def diana(x,y):
+    import math
+    if ( x < 5 or x >= 25 ) and ( y < 10 or y >= 30 ):
+        return (3)
+    else:
+        if ( x < 5 or x >= 25 ) and ( y >= 10 and y < 30 ):
+            return (5)
+        else:
+            if ( x >= 5 and x < 25 ) and ( y < 10 or y >= 30 ):
+                return (7)
+            else:
+                if pow ( x-15. ,2 ) + pow ( x-20. ,2 ) < 100:
+                    return (10)
+                else:
+                    return (100) 
