5d7853e33b9f9d32860491a8a7e8501d202bb965
310126070 <brenner227@hotmail.com>
2019-01-17 17:08:07 -0600

    Clase

--- Clases/Programas/Problema1.py
+++ Clases/Programas/Problema1.py
@@ -7,7 +7,26 @@ def mcd(m,n):
     else:
         return mcd(menor, resto)
 
+
+
+def MCD(a,b):
+    if a<b:
+        tmp=b
+        b=a
+        a=tmp
+    r=a%b
+    while r!=0:
+        a=b
+        b=r
+        r=a%b
+    return b
+
+
+
+
 print mcd(45,60)
+print MCD(60,45)
 # 15
 print mcd(100,140)
+print MCD(100,140)
 # 20
