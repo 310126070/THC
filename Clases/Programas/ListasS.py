93d9f1739a7ef74517cf92c0398823257e7e638b
310126070 <brenner227@hotmail.com>
2019-01-21 11:12:04 -0600

    Métodos

---
+++ Clases/Programas/ListasS.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+'''
+Moctezuma Soto, Fernando
+310126070
+Taller de herramientas computacionales
+
+Impresión de los métodos de listas
+'''
+
+print '''
+
+ list. append ( x )
+
+    Añade un elemento al final de la lista.
+    
+
+ list.count( x )
+
+    Devuelve el número de veces que x aparece en la lista.
+    
+
+ list.extend( L )
+
+    Amplía la lista agregando todos los elementos de la lista dada.
+
+ list.index( x )
+
+    Devuelve el índice en la lista del primer elemento cuyo valor es x.
+    Es un error si no hay tal artículo.
+
+ list.insert( i , x )
+
+    Inserta un elemento en una posición dada. El primer argumento es el
+    índice del elemento antes del cual se inserta.
+
+ list.pop( [ i ] )
+
+    Elimina el elemento en la posición dada en la lista y lo devulve.
+    Si no se especifica ningún índice, a.pop() elimina y devuelve el
+    último elemento de la lista. (Los corchetes alrededor de la i
+    indican que el parámetro es opcional, no que se deben escribir
+    corchetes en esa posición).
+
+ list.remove ( x )
+
+    Elimina el primer elemento de la lista cuyo valor es x.
+    Es un error si no hay tal elemento. 
+
+ list.reverse( )
+
+    Invierte los elementos de la lista, en su lugar.
+                                                                                                                                                                     
+ list.sort( cmp = None , key = None , reverse = False )
+
+    Ordena los elementos de la lista en su lugar.    '''                                                                                                                    
