2fc1ffc226186681bcd0bed0ce679827a4f95f42
310126070 <brenner227@hotmail.com>
2019-01-25 16:58:18 -0600

    Examen

---
+++ Clases/Programas/Practica/Problema7.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+'''
+Moctezuma Soto, Fernando
+310126070
+Taller de herramientas computacionales
+
+Programa de la función
+'''
+
+
+from math import sin, pi
+
+def H_eps(x,eps=0.01):
+    if x < -eps:
+        return 0
+    elif x >= -eps and x <= eps:
+        return 1/2.0 + x/(2*eps) + 1/(2*eps)*(sin(pi*x/eps))
+    elif x > eps:
+         return 1
---
+++ Clases/Programas/Practica/Problema7S.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+'''
+Moctezuma Soto, Fernando
+310126070
+Taller de herramientas computacionales
+
+Programa de la función
+'''
+
+
+from Problema7 import H_eps
+
+x = input("Introducir primer número: ")
+
+
+print "El %d" % (H_eps(x,eps=0.01))
